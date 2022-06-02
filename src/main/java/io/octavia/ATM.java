d9990b0af324c668be5e3c1af22dcb2f54f7c0bd
lifeoncode <ndu918@outlook.com>
2022-06-02 17:39:00 +0200

    runnable ATM

---
+++ src/main/java/io/octavia/ATM.java
@@ -0,0 +1,7 @@
+package io.octavia;
+
+public class ATM {
+    public static void main(String[] args) {
+        System.out.println("Running...");
+    }
+}
---
+++ src/main/java/io/octavia/Account.java
@@ -0,0 +1,6 @@
+package io.octavia;
+
+public class Account {
+    private String accountNumber;
+    private String accountHolder;
+}
