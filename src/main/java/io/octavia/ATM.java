78e7116bd00d1875876083448bc67a55fe227a4c
lifeoncode <ndu918@outlook.com>
2022-06-04 17:49:45 +0200

    Prompting user for account creation
    if user does not have an account, they must create one

--- src/main/java/io/octavia/ATM.java
+++ src/main/java/io/octavia/ATM.java
@@ -5,13 +5,49 @@ import java.util.Scanner;
 
 public class ATM {
     public static void main(String[] args) {
-        System.out.println("Running...");
-
-        System.out.print("Account number: ");
         Scanner scanner = new Scanner(System.in);
-        String AccNumber = scanner.nextLine();
 
-        boolean validAccNumber = new Bank(AccNumber).validateAccNumber();
+
+        if (!haveAccount(scanner)) {
+            createAccount(scanner);
+        } else {
+            System.out.println("account holder: ");
+        }
+//        boolean validAccNumber = new Bank(AccNumber).validateAccNumber();
+    }
+
+    static boolean haveAccount(Scanner scanner) {
+        System.out.print("Do you have an account: ");
+        String haveAccount = scanner.nextLine();
+
+        if (haveAccount.equalsIgnoreCase("yes")) {
+            return true;
+        }
+        return false;
+    }
+
+    static void createAccount(Scanner scanner) {
+        ValidateInput validate = new ValidateInput();
+        String name;
+        String surname;
+
+        while(true) {
+            System.out.print("name: ");
+            name = scanner.nextLine();
+            if (validate.validateName(name)) {
+                break;
+            }
+        }
+
+        while(true) {
+            System.out.print("surname: ");
+            surname = scanner.nextLine();
+            if (validate.validateName(surname)) {
+                break;
+            }
+        }
+        String info = String.format("NAME :: %s\nSURNAME :: %s",name, surname);
+        System.out.println(info);
 
     }
 
